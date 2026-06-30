0c4209138b0371a696ca363ebb462937a991f2c7
Felipe Souza <felipessouza382@gmail.com>
2026-06-30 16:23:25 -0300

    test(backend): configura ambiente do jest para e2e e coverage

---
+++ backend/jest-e2e.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/actions/**/*.(t|j)s',
+    '!src/**/*.module.ts',
+    '!src/**/*.dto.ts',
+  ],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 90,
+      functions: 95,
+      lines: 95
+    }
+  }
+};
